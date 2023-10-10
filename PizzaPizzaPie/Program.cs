220835743e53031a7e5995b45fd4b5006c05769f
Jhone Gomes <g.m.enohj@gmail.com>
2023-10-10 16:43:53 -0300

    Inject PizzaService dependence.

--- PizzaPizzaPie/Program.cs
+++ PizzaPizzaPie/Program.cs
@@ -1,3 +1,5 @@
+using PizzaPizzaPie.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddScoped<PizzaService>();
 
 var app = builder.Build();
 
@@ -21,5 +25,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapGet("/", () => @"Pizza Pizza Pie management API. Navigate to /swagger to open the Swagger test UI.");
 
 app.Run();
