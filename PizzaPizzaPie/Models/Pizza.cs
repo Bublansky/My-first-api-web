e0418271daf2913d3c310012a1d70ea9818ca830
Jhone Gomes <g.m.enohj@gmail.com>
2023-10-10 15:37:04 -0300

    Add Sauce and Topping models.

--- PizzaPizzaPie/Models/Pizza.cs
+++ PizzaPizzaPie/Models/Pizza.cs
@@ -5,4 +5,6 @@ public class Pizza
     public int Id { get; set; }
     public string? Name { get; set; }
     public bool IsGlutenFree { get; set; }
+    public Sauce? Sauce { get; set; }
+    public ICollection<Topping>? Toppings { get; set; }
 }
---
+++ PizzaPizzaPie/Models/Topping.cs
@@ -0,0 +1,7 @@
+namespace PizzaPizzaPie.Models;
+
+public class Topping
+{
+    public int Id { get; set; }
+    public string? Name { get; set; }
+}
